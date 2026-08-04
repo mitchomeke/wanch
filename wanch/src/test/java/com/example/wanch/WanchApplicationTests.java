20c7f366d77a85d8f9af5d988cb8515a11810970
Mitchell Chukwueku Omeke <164729363+mitchomeke@users.noreply.github.com>
2026-08-04 11:32:30 +0100

    Initial commit

---
+++ wanch/src/test/java/com/example/wanch/WanchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wanch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
